4006f181381b9315ebab61f46b84074174f5accf
Lucas <lucas.santos@mundowap.com.br>
2023-01-02 20:39:58 -0300

    fix: vite import

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     host: true,
   },
