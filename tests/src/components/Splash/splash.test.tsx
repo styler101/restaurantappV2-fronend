3c43b395da1a740d6a379853a61c589c63a73559
Lucas <lucas.santos@mundowap.com.br>
2023-01-05 07:21:46 -0300

    test: make sut splash component

--- tests/src/components/Splash/splash.test.tsx
+++ tests/src/components/Splash/splash.test.tsx
@@ -2,13 +2,30 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { Splash } from '@/components/Splash'
 
+interface SutProps {
+  sut: SplashSub
+}
+class SplashSub {
+  mock() {
+    return {
+      visible: true,
+      setVisible: () => true,
+    }
+  }
+}
+
+const makeSut = () => {
+  return new SplashSub().mock()
+}
 describe('Splash Component', () => {
   test('Should be render Splash Component', () => {
-    render(<Splash />)
+    const sut = makeSut()
+    render(<Splash isVisible={sut.visible} setIsVisible={sut.setVisible} />)
   })
 
   test('Should not render Splash Component on the second render', async () => {
-    render(<Splash />)
+    const sut = makeSut()
+    render(<Splash isVisible={sut.visible} setIsVisible={sut.setVisible} />)
     await setTimeout(() => {
       const wrapper = screen.queryByTestId('splash-container')
       expect(wrapper).not.toBeInTheDocument()
--- tests/src/pages/SignIn/signin.spec.tsx
+++ tests/src/pages/SignIn/signin.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { Signin } from '@/pages/Signin/index'
+import { Signin } from '@/pages/Signin'
 import { Splash } from '@/components/Splash'
 
 describe('SignIn Component', () => {
@@ -9,6 +9,13 @@ describe('SignIn Component', () => {
   })
 
   test('Shoul render Splash Component', () => {
-    render(<Splash />)
+    render(
+      <Splash
+        isVisible={true}
+        setIsVisible={() => {
+          return true
+        }}
+      />
+    )
   })
 })
