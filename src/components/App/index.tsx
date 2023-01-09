663dcca742536c6e6a5449271e9df66fe353b731
Lucas <lucas.santos@mundowap.com.br>
2023-01-09 09:04:44 -0300

    chore: add react-toastify

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
 import { ThemeProvider } from 'styled-components'
-import { Signin } from '@/pages/Signin/index'
+import { Routes } from '@/routes'
 import GlobalStyles from '@/assets/styles/global'
 import theme from '@/assets/theme'
 
 export function App() {
   return (
     <ThemeProvider theme={theme.light}>
-      <Signin />
+      <Routes />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        pauseOnHover
+        theme="light"
+      />
+
       <GlobalStyles />
     </ThemeProvider>
   )
