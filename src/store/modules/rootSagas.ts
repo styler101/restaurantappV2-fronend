56ffefe0af9979b32dc2d1f7a39d60a163484523
Lucas <lucas.santos@mundowap.com.br>
2023-01-09 21:21:12 -0300

    refactor: remove redux as a manager statement

--- src/store/modules/rootSagas.ts
+++
@@ -1,6 +0,0 @@
-import { all } from 'redux-saga/effects'
-import auth from './auth/sagas'
-// As nossas sagas serão importadas aqui
-export default function* rootSaga(): any {
-  return yield all([auth])
-}
--- src/store/storeTypes.ts
+++
@@ -1,7 +0,0 @@
-import { AuthActions, AuthState } from '@/store/modules/auth/types'
-
-export interface StoreState {
-  auth: AuthState
-}
-
-export type StoreAction = AuthActions
