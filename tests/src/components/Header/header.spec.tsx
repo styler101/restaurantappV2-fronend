61351d4eba3c80fedf849cdf2bc42aa7aba54b8e
Lucas <lucas.santos@mundowap.com.br>
2023-01-12 09:26:16 -0300

    test: ensure the header component recieve a correct title

--- tests/src/components/Header/header.spec.tsx
+++ tests/src/components/Header/header.spec.tsx
@@ -5,11 +5,22 @@ import { Header } from '@/components/Header'
 describe('Header Component', () => {
   test('Ensure the header component recieve a title', () => {
     const fakeProps = {
-      title: 'any_title',
+      title: '',
       description: 'any_description',
       icon: 'fake_icon',
     }
     render(<Header data={fakeProps} />)
+  })
+
+  test('Ensure the header component recieve a correct title', () => {
+    const fakeProps = {
+      title: 'anyTitle',
+      description: 'any_description',
+      icon: 'fake_icon',
+    }
+    render(<Header data={fakeProps} />)
+
+    expect(fakeProps.title).toBeTruthy()
   })
 
   test('Ensure the header componente recieve a description', () => {
