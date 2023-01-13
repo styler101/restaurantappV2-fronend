aa9e50a51c3dc65e8653156e102c831ce156d8e2
Lucas <lucas.santos@mundowap.com.br>
2023-01-13 06:57:30 -0300

    test: ensure the header component recieve a correct description

--- tests/src/components/Header/header.spec.tsx
+++ tests/src/components/Header/header.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import { Header } from '@/components/Header'
 
 describe('Header Component', () => {
-  test('Ensure the header component recieve a title', () => {
+  test('ensure the header component recieve a title', () => {
     const fakeProps = {
       title: '',
       description: 'any_description',
@@ -12,7 +12,7 @@ describe('Header Component', () => {
     render(<Header data={fakeProps} />)
   })
 
-  test('Ensure the header component recieve a correct title', () => {
+  test('ensure the header component recieve a correct title', () => {
     const fakeProps = {
       title: 'anyTitle',
       description: 'any_description',
@@ -23,12 +23,22 @@ describe('Header Component', () => {
     expect(fakeProps.title).toBeTruthy()
   })
 
-  test('Ensure the header componente recieve a description', () => {
+  test('ensure the header component recieve a description', () => {
+    const fakeProps = {
+      title: 'any_title',
+      description: '',
+      icon: 'fake_icon',
+    }
+    render(<Header data={fakeProps} />)
+  })
+
+  test('ensure the header component recieve a correct description', () => {
     const fakeProps = {
       title: 'any_title',
       description: 'any_description',
       icon: 'fake_icon',
     }
     render(<Header data={fakeProps} />)
+    expect(fakeProps.description).toEqual('any_description')
   })
 })
