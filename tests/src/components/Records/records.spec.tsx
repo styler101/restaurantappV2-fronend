d0d471dd8ad67f3d25f13cedb6a1e888d85231fb
Lucas <lucas.santos@mundowap.com.br>
2023-01-28 14:22:51 -0300

    refactor: move SUT creation to a factory method

--- tests/src/components/Records/records.spec.tsx
+++ tests/src/components/Records/records.spec.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import { Records } from '@/components/Records'
+import { HeaderProps } from '@/components/Records/interfaces'
 import { render } from '@testing-library/react'
 
+type SutTypes = {
+  header: HeaderProps[]
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    header: [{ label: 'any_label', content: 'any_content' }],
+  }
+}
 describe('Records Component', () => {
   test('Ensure that records component receive a header', () => {
-    const header = [{ label: 'any_label', content: 'any_content' }]
+    const { header } = makeSut()
     render(<Records header={header} />)
     expect(header).not.toBe(null)
   })
 
-  test('Ensure that records component receive a correct props', () => {
-    const header = [{ label: 'any_content', content: 'any_content' }]
+  test('Ensure that header props receive a correct props', () => {
+    const { header } = makeSut()
     render(<Records header={header} />)
     expect(header).toEqual(
       expect.arrayContaining([
