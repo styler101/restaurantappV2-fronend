42ffa3d3c4c86cf351acdf290ff33afb4aa60a8c
Lucas <lucas.santos@mundowap.com.br>
2023-02-01 22:09:35 -0300

    test: ensure that title component has a heading 1

--- tests/src/components/Title/title.spec.tsx
+++ tests/src/components/Title/title.spec.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Title } from '@/components/Title'
 import { TitleProps } from '@/components/Title/interfaces'
 import { faker } from '@faker-js/faker'
+import '@testing-library/jest-dom'
 
 type SutTypes = {
   sut: TitleProps
 }
-const makeFakeProps = (): SutTypes => {
+const makeSut = (): SutTypes => {
   const makeProps = {
     title: faker.name.jobDescriptor(),
     quantity: faker.internet.httpStatusCode(),
@@ -18,8 +19,21 @@ const makeFakeProps = (): SutTypes => {
 }
 describe('Title Component', () => {
   test('ensure title component recieve a valid title', () => {
-    const { sut } = makeFakeProps()
+    const { sut } = makeSut()
     render(<Title data={sut.data} />)
     expect(sut.data.title).not.toBe('')
+  })
+
+  test('ensure title component recieve a valid title', () => {
+    const { sut } = makeSut()
+    render(<Title data={sut.data} />)
+    expect(sut.data.title).not.toBe(null)
+  })
+
+  test('ensure that title component has a heading 1', () => {
+    const { sut } = makeSut()
+    render(<Title data={sut.data} />)
+    const heading = screen.getByRole('heading', { level: 1 })
+    expect(heading).toBeInTheDocument()
   })
 })
